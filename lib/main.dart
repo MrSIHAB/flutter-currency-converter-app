40558bc7595b295c72e8ee300dbfd28bc5e9da91
SIHAB <MrSIHAB@hotmail.com>
2024-08-17 23:34:34 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_corrency_converter/pages/corency_converter_material_page.dart';
-import 'package:flutter_corrency_converter/pages/corrency_converter_cupertino_page.dart';
 
 void main() {
   runApp(const MyApp());
